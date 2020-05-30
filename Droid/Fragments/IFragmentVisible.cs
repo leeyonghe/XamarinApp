279954e90daf7512127577127881518689477db3
lee <lyhdra99@svcorps.dev>
2020-05-30 21:15:44 +0900

    init

---
+++ Droid/Fragments/IFragmentVisible.cs
@@ -0,0 +1,7 @@
+﻿namespace ExampleMultiApp.Droid
+{
+    interface IFragmentVisible
+    {
+        void BecameVisible();
+    }
+}
---
+++ Droid/Helpers/RecyclerClickEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Android.Views;
+
+namespace ExampleMultiApp.Droid
+{
+    public class RecyclerClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
